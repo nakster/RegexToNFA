d3cc2bc7fb07f6c18aeee35c0c37ff61282dd811
nakster <g00332403@gmit.ie>
2018-03-24 22:52:25 +0000

    added the input option

--- Project/main.go
+++ Project/main.go
@@ -7,4 +7,17 @@ import (
 func main() {
 
 	fmt.Println("start of the project")
+	fmt.Print("Please Enter \n 1) For infix Expressions Conversion To NFA\n 2) for postFix Expressions Conversion To NFA \n 3) To Exit\n")
+	var input int
+	fmt.Scanln(&input)
+
+	switch input {
+	case 1:
+		fmt.Println("Option", input, "Was Entered !!!")
+	case 2:
+		fmt.Println("Option", input, "Was Entered !!!")
+	default:
+		fmt.Println("Enter on of the following options")
+	}
+
 }

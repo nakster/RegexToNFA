d10d005d892d0a8058213093a7f2873df0804a85
nakster <g00332403@gmit.ie>
2018-03-25 21:47:00 +0100

    * added

--- Project/src/util/nfa.go
+++ Project/src/util/nfa.go
@@ -45,6 +45,16 @@ func ReturnNFA(pofix string) *nfa {
 
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
 		case '*':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
 
 		default:
 
